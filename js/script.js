31c92af26185a8e841d0936c54eac4b5b16f5103
Abdyl-Aziz <azizshihabov@mail.ru>
2021-08-10 22:43:08 +0200

    feat: add some blocks

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 const firstBlock = document.querySelector('.first-block'),
-      firstBtn = document.querySelector('.main__btn'),
-      secondBlock = document.querySelector('.second-block'),
-      secondBtn = document.querySelector('.second-btn'),
-      landingBlock = document.querySelector('.landing-block'),
-      CounterPlus = document.getElementById('counter-plus'),
-      CounterMinus = document.getElementById('counter-minus'),
-      CounterInput = document.getElementById('counter-number')
-      ;
+  firstBtn = document.querySelector('.main__btn'),
+  secondBlock = document.querySelector('.second-block'),
+  secondBtn = document.querySelector('.second-btn'),
+  landingBlock = document.querySelector('.landing-block'),
+  CounterPlus = document.querySelectorAll('.counter-plus'),
+  CounterMinus = document.querySelectorAll('.counter-minus'),
+  CounterInput = document.querySelectorAll('.counter-number'),
+  inputRadios = document.querySelectorAll('.second-block__input-block input'),
+  landingBtn = document.querySelector('.landing-btn'),
+  multipleBtn = document.querySelector('.multiple-bth'),
+  magazineBtn = document.querySelector('.magazine-bth'),
+  totalPrice = document.querySelector('.total-price'),
+  blockPrice = 700;
 
 
-function hideElement(elem){
+
+
+
+function hideElement(elem) {
   elem.classList.remove('show');
   elem.classList.add('hide');
 }
@@ -23,30 +31,53 @@ firstBtn.addEventListener('click', () => {
   showElement(secondBlock);
 });
 secondBtn.addEventListener('click', () => {
+
+
+  const inputChecked = document.querySelector('input:checked');
+
+  const inputValue = inputChecked.value;
+
+
+
   hideElement(secondBlock);
-  showElement(landingBlock);
+  showElement(document.querySelector('#' + inputValue));
 });
 
 
 
-CounterPlus.addEventListener('click', () => {
-  CounterInput.value = Number(CounterInput.value) + 1;
+CounterPlus.forEach(item => {
+  item.addEventListener('click', () => {
+    CounterInput.forEach(item => {
+      item.value = Number(item.value) + 1;
 
-  CounterInput.style.background = '#ff9900';
-  setTimeout(() =>{
-    CounterInput.style.background = '#fff';
-  }, 100);
+      item.style.background = '#ff9900';
+      setTimeout(() => {
+        item.style.background = '#fff';
+      }, 100);
+    });
+  })
 });
 
-CounterMinus.addEventListener('click', () => {
-  if(CounterInput.value === '1') {
-    return;
-  } else {
-    CounterInput.value = Number(CounterInput.value) - 1;
-    CounterInput.style.background = '#ff9900';
-    setTimeout(() =>{
-      CounterInput.style.background = '#fff';
-    }, 100);
-  }
+CounterMinus.forEach(item => {
+  item.addEventListener('click', () => {
+    CounterInput.forEach(item => {
+      if (item.value === '1') {
+        return;
+      } else {
+        item.value = Number(item.value) - 1;
+        item.style.background = '#ff9900';
+        setTimeout(() => {
+          item.style.background = '#fff';
+        }, 100);
+      }
+    });
+  });
 });
 
+landingBtn.addEventListener('click', () => {
+  const landingInput = document.querySelector('.landing-number');
+  const landingValue = landingInput.value;
+  const totalValue = landingValue * blockPrice;
+  totalPrice.innerHTML = `Цена: ${totalValue}р`;
+
+});
