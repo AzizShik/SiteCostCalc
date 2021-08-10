d54a97431143f9f8be498b2116d45250dea68354
Abdyl-Aziz <azizshihabov@mail.ru>
2021-08-10 20:32:26 +0200

    feat: add landing block

--- js/script.js
+++ js/script.js
@@ -2,7 +2,11 @@ const firstBlock = document.querySelector('.first-block'),
       firstBtn = document.querySelector('.main__btn'),
       secondBlock = document.querySelector('.second-block'),
       secondBtn = document.querySelector('.second-btn'),
-      thirdBlock = document.querySelector('.third-block');
+      landingBlock = document.querySelector('.landing-block'),
+      CounterPlus = document.getElementById('counter-plus'),
+      CounterMinus = document.getElementById('counter-minus'),
+      CounterInput = document.getElementById('counter-number')
+      ;
 
 
 function hideElement(elem){
@@ -20,5 +24,29 @@ firstBtn.addEventListener('click', () => {
 });
 secondBtn.addEventListener('click', () => {
   hideElement(secondBlock);
-  showElement(thirdBlock);
+  showElement(landingBlock);
 });
+
+
+
+CounterPlus.addEventListener('click', () => {
+  CounterInput.value = Number(CounterInput.value) + 1;
+
+  CounterInput.style.background = '#ff9900';
+  setTimeout(() =>{
+    CounterInput.style.background = '#fff';
+  }, 100);
+});
+
+CounterMinus.addEventListener('click', () => {
+  if(CounterInput.value === '1') {
+    return;
+  } else {
+    CounterInput.value = Number(CounterInput.value) - 1;
+    CounterInput.style.background = '#ff9900';
+    setTimeout(() =>{
+      CounterInput.style.background = '#fff';
+    }, 100);
+  }
+});
+
